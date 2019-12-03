3fc19429fd605ad3a597a97a4fd3859bc3d80997
Ethan Kieser <53837375+ekieser@users.noreply.github.com>
2019-12-03 14:53:30 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const api_key = 'YyQ7clRRUWhfH2oeY88CtZ5WFalJRN2nSDKMrskJ';
+
+const searchURL = 'https://developer.nps.gov/api/v1/parks'
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function retrieveParks(query, limit=10) {
+    const params = {
+        key: api_key,
+        stateCode: query,
+        limit: 10,
+    };
+    const queryString = formatQueryParams(params);
+    const url = searchURL + '?' + queryString;
+    fetch (url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => console.log(JSON.stringify(responseJson)))
+        .catch(err => {
+            $('#js-error-message').text(`There was an error: ${err.message}`);
+        });
+}
+
+function displayParks(responseJson) {
+    console.log(responseJson);
+    for (let i = 0; i < responseJson.length; i++) {
+        $('#display-results').append(
+            `<a href='${responseJson.data[i].url}' target="blank"><h3>${responseJson.data[i].fullname}</h3></a>
+            <h4>Location: ${responseJson.data[i].states}</h4>
+            <p>${responseJson.data[i].description}</p>
+            <a href="${responseJson.data[i].directionsUrl}" target="blank">Directions</a>`
+        )
+    };
+}
+
+function startSearch() {
+    $('form').submit(event => {
+        event.preventDefault();
+       $('#display-results').html('');
+       $('#js-error-message').html('');
+        const searchTerm = $('#state-entry').val();
+        const maxResults = $('#results-entry').val();
+        retrieveParks(searchTerm, maxResults);
+    });
+}
+
+$(startSearch);
